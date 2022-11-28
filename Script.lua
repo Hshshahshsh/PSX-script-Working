42e6dfe5d1ad30360b484af846e9a7a08a3dbb3d
Hshshahshsh <110442121+Hshshahshsh@users.noreply.github.com>
2022-11-28 20:53:08 +0800

    Delete Script.lua

--- Script.lua
+++
@@ -1,3 +0,0 @@
-_G.License = "3a706b644f0e5dda1c5c"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script.lua"))()
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/jmesfo0/RobloxScripts/main/psx-jmes.lua"))()
